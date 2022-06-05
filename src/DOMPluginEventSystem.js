ddb940c8e242e0527235c6a5953bf05823927573
dezhizhang <1541609448@qq.com>
2022-06-05 21:25:17 +0800

    feat:update

--- src/DOMPluginEventSystem.js
+++ src/DOMPluginEventSystem.js
@@ -1,6 +1,8 @@
 
 import { allNativeEvents } from './EventRegister';
-import * as SimpleEventPlugin  from './SimpleEventPlugin';
+import * as SimpleEventPlugin from './SimpleEventPlugin';
+import { getEventListenerSet } from './ReactDOMComponentTree';
+import { addEventBubbleListener, addEventCaptureListener } from './EventListener';
 
 //注册事件名称
 SimpleEventPlugin.registerEvents();
@@ -9,15 +11,35 @@ export const nonDelegatedEvents = new Set(['scroll']);
 
 //临听所有的绑定的插件
 export function listenToAllSupportedEvents(container) {
-    allNativeEvents.forEach(domEventName=> {
-        if(!nonDelegatedEvents.has(domEventName)) {
-            listenToNativeEvent(domEventName,false,container);
+    allNativeEvents.forEach(domEventName => {
+        if (!nonDelegatedEvents.has(domEventName)) {
+            listenToNativeEvent(domEventName, false, container);
         }
-        listenToNativeEvent(domEventName,true,container);
-        console.log('domEventName',domEventName);
+        listenToNativeEvent(domEventName, true, container);
     })
 }
 
-function listenToNativeEvent(domEventName,isCapturePhaseListener,rootContainerElement) {
-    
+function listenToNativeEvent(domEventName, isCapturePhaseListener, rootContainerElement, eventSystemFlags = 0) {
+    let listenerSet = getEventListenerSet(rootContainerElement);
+    let listenerSetKey = getListenerSetKey(domEventName, isCapturePhaseListener);
+    if (listenerSet.has(listenerSetKey)) {
+        addTrappedEventListener(
+            rootContainerElement,
+            domEventName,
+            eventSystemFlags
+        )
+    }
+}
+
+function addTrappedEventListener(rootContainerElement, domEventName, eventSystemFlags, isCapturePhaseListener) {
+    let listener;
+    if (isCapturePhaseListener) {
+        addEventCaptureListener(rootContainerElement, domEventName, listener);
+    } else {
+        addEventBubbleListener(rootContainerElement, domEventName, listener);
+    }
+}
+
+function getListenerSetKey(domEventName, isCapturePhaseListener) {
+    return `${domEventName}__${isCapturePhaseListener ? 'capture' : 'bubble'} `;
 }
---
+++ src/EventListener.js
@@ -0,0 +1,9 @@
+
+
+export function addEventBubbleListener(target,eventType,listener) {
+    target.addEventListener(eventType,listener,false);
+}
+
+export function addEventCaptureListener(target,eventType,listener) {
+    target.addEventListener(eventType,listener,true)
+}
